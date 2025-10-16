0d261e3b1c2f6ff6601b2180e55e690134494852
Felipe Cortes <felipe_cortes@epam.com>
2025-10-16 17:14:57 -0500

    Initial commit on carlos-main branch

--- src/dunders/dunders_minimal_case.py
+++ src/dunders/dunders_minimal_case.py
@@ -1,16 +1,24 @@
 """Minimal example for dunders."""
 
+
 class MyObject:
     def __init__(self, value):
-        self.value = value
+        # Garantiza que el valor sea convertible a entero
+        self.value = int(value)
 
     def __add__(self, right_obj):
-        return MyObject(self.value + right_obj.value)
+        # Concatenar como strings y convertir a entero
+        concatenated = int(str(self.value) + str(right_obj.value))
+        return MyObject(concatenated)
 
-if __name__ == '__main__':
+    def __repr__(self) -> str:
+        # Devuelve representación como entero en forma de string, solo para ser impreso
+        return str(self.value)
+
+
+if __name__ == "__main__":
     left_obj = MyObject(1)
-    print(left_obj)
+    print(left_obj)  # Output: 1
     right_obj = MyObject(2)
-    print(left_obj)
-    print(right_obj)
-    print(left_obj + right_obj)
+    print(right_obj)  # Output: 2
+    print(left_obj + right_obj)  # Output: 12
